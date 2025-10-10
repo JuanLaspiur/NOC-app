c5f666b67be8e5504e9a47026b58fe6f1148ca8a
Juan Laspiur <laspiurperezjuan@gmail.com>
2025-10-10 14:11:16 -0300

    save

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
 
 export class Server {
@@ -6,8 +7,7 @@ export class Server {
         CronService.createJob({
            cronTime: '*/5 * * * * *',
             onTick:()=>{
-                let date = new Date()
-                console.log('5 seconds '+date)
+               new CheckService().execute('https://google.com')
             },
             timeZone :'America/Argentina/Buenos_Aires'},
 
